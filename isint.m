377122d167263b824059a3c614174b8ca25e0570
Hani Andreas Ibrahim <hani.ibrahim@gmx.de>
2016-04-24 21:37:26 +0200

    Bugs in isint.m fixed

--- isint.m
+++ isint.m
@@ -1,11 +1,18 @@
-% Check whether the committed NUM is a integer (postive and/or negative). 
+% Checks whether the committed NUM is a integer. You can specify if only
+% positive or postive and negative integers are accepted as well as zero is 
+% a valid integer or not. 
+%
+% USAGE
+%        int = isint(num)
+%        int = isint(num, onlypos)
+%        int = isint(num, onlypos, z)
 %  
 % INPUT:
 %        num     : Number to be checked
 %        onlypos : If 0 (default): Checks for positive and negative integers
 %                  If 1: Checks only for positive integers incl. 0.
-%        z       : If 0: Checks for integers w/o 0 
-%                  If 1 (default): Checks integers w/ 0
+%        z       : If 0: Checks for integers w/o zero (0) 
+%                  If 1 (default): Checks integers w/ zero (0)
 %
 % OUTPUT:
 %        int     : TRUE (1) if num is an integer
@@ -40,21 +47,20 @@ function int = isint(num, onlypos, z)
   if nargin < 3; z = true; endif
   if ~isnumeric(num); error("First argument has to be numeric"); endif
   if (onlypos != false && onlypos != true) || (z != false && z != true) 
-    error("Second (and third) argument has to be 1 or 0"); 
+    error("Second (and third) argument has to be 1 or 0, true or false resp."); 
   endif
   
-  if (isnumeric(num) && (num-floor(num) == 0) && onlypos == 0 && z == 1)
+  if isnumeric(num) && (num-floor(num) == 0) && onlypos == false && z == true
     int = true;
-  elseif (isnumeric(num) && (num-floor(num) == 0) && onlypos == 0) && ...
-         (z == 0 && num == 0)
-    int = false;
-  elseif (isnumeric(num) && (num-floor(num) == 0) && onlypos == 0) && ...
-         (z == 0 && num != 0)
+  elseif isnumeric(num) && (num-floor(num) == 0) && onlypos == false && ...
+         z == false && num != 0
+    int = true;
+  elseif isnumeric(num) && (num-floor(num) == 0) && onlypos == true && ...
+         num > 0 && z == false
     int = true;;
-  elseif (isnumeric(num) && (num-floor(num) == 0) && onlypos == 1 && num >= 0)
+  elseif isnumeric(num) && (num-floor(num) == 0) && onlypos == true && ...
+         z == 1 && num >= 0
     int = true;
-  elseif (isnumeric(num) && (num-floor(num) == 0) && onlypos == 1 && num < 0)
-    int = false;
   else
     int = false;
   endif 
