f708f8065a980828580e92691b3a608510f4ce53
Hani Andreas Ibrahim <hani.ibrahim@gmx.de>
2016-05-02 00:54:45 +0200

    Fix bug in afterdecimal()

--- afterdecimal.m
+++ afterdecimal.m
@@ -10,23 +10,26 @@
 % EXAMPLE
 %
 %      afterdecimal(12.23)
-%      => 0.23
+%      => 0.23000
 %
 %      afterdecimal(-12.23)
-%      => 0.23
+%      => 0.23000
 
 function x = afterdecimal(num)
   if nargin < 1 || nargin > 1; error("Commit one decimal number"); endif
   if ~isnumeric(num); error("Only decimal number is allowed!"); endif
+  
+  if num < 0; num = num * -1; endif
+  x = num - floor(num); 
 
-  xstr       = num2str(num); % Make number to string
-  comma_idx  = index(xstr, "."); % Find index of the decimal point in the string
-  if comma_idx == 0
-    x = 0;
-  else
-    sub = substr(xstr, comma_idx); % Cut the chars up to the decimal point
-    x   = str2num(sub); % Convert string back to a number
-  endif
+%  xstr       = num2str(num,100); % Make number to string
+%  comma_idx  = index(xstr, "."); % Find index of the decimal point in the string
+%  if comma_idx == 0
+%    x = 0;
+%  else
+%    sub = substr(xstr, comma_idx); % Cut the chars up to the decimal point
+%    x   = str2num(sub); % Convert string back to a number
+%  endif
   
   return;
   
